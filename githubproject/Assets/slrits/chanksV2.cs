52abbc411aaf52cfe7cbe184ecb906c592ff0afd
Tikhon2007 <75084387+Tikhon2007@users.noreply.github.com>
2022-03-19 15:08:41 +0300

    error

---
+++ githubproject/Assets/slrits/chanksV2.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chanksV2 : MonoBehaviour
+{
+    public Transform chank1;
+    public Transform chank2;
+    public Transform chank3;
+    public float aabbccd = 25;
+    public Vector3 vpered;
+    async void FixedUpdate()
+    {
+        int a = Random.Range(1, 3);
+        
+    }
+}
