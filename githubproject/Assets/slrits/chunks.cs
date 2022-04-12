afda8df3a5082b49c2f5bfb96d7a99ebf2b6d6e7
Tikhon2007 <75084387+Tikhon2007@users.noreply.github.com>
2022-04-12 19:37:56 +0300

    not

--- githubproject/Assets/slrits/chunks.cs
+++ githubproject/Assets/slrits/chunks.cs
@@ -1,21 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
 
 public class chunks : MonoBehaviour
 {
-    public float aabbcc = 40;
-    public float aabb = 120;
-    public Vector3 vpered;
+    public float otigroka = 40;
+    public float spavn = 120;
+    public Vector3 vpered ;
+    public List<GameObject> chunk = new List<GameObject>();
 
     void FixedUpdate()
     {
-        if (GameObject.FindWithTag("chunk"))
-        {
-            if (transform.position.z + aabbcc> GameObject.FindWithTag("chunk").transform.position.z)
-            {
-                Destroy(GameObject.FindWithTag("chunk"));
+        int randomNumber = Random.Range(0,chunk.Count);
+        for(int i = 1; i < chunk.Count; i++){
+            if(transform.position.z-otigroka>chunk[i].transform.position.z){
+                Vector3 spawnPosition = new Vector3(vpered.x, vpered.y, transform.position.z + vpered.z);
+                Instantiate(chunk[randomNumber], vpered, Quaternion.identity);
+                
             }
         }
 
-
+        
     }
 }
+
+
+
+
